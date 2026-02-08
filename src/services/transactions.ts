ae2d4a51f75f4bbcc05b54db601cd2bb9edc55d4
Diego E <diego28e@gmail.com>
2026-02-08 16:40:04 -0500

    feat:add transaction service

---
+++ src/services/transactions.ts
@@ -0,0 +1,29 @@
+import {apiClient} from '../lib/api';
+import type {Transaction, CreateTransactionRequest} from '../types/api';
+
+export const transactionService = {
+    async getUserTransacions(userId: string): Promise<Transaction[]> {
+        return apiClient<Transaction[]>(`/users/${userId}/transactions`);
+    },
+    async getTransactionById(id:string):Promise<Transaction> {
+        return apiClient<Transaction>(`/transactions/${id}`);
+    },
+    async createTransaction(data: CreateTransactionRequest): Promise<Transaction> {
+        return apiClient<Transaction>('/transactions/', {
+            method: 'POST',
+            body: JSON.stringify(data),
+        });
+    },
+    async updateTransaction(id:string, data: Partial<CreateTransactionRequest>): Promise<Transaction> {
+        return apiClient<Transaction>(`/transaction/${id}`,{
+            method: 'PUT',
+            body: JSON.stringify(data),
+        });
+    },
+
+    async deleteTransaction(id:string):Promise<void> {
+        await apiClient(`/transactions/${id}`, {
+            method: 'DELETE',
+        });
+    }
+}
